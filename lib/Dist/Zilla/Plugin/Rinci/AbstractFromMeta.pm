5a3d695c69be9b44c82a61fd101bd35f39d57c02
perlancar (@pc-home) <perlancar@gmail.com>
2014-12-14 08:12:58 +0700

    Fixes

--- lib/Dist/Zilla/Plugin/Rinci/AbstractFromMeta.pm
+++ lib/Dist/Zilla/Plugin/Rinci/AbstractFromMeta.pm
@@ -11,13 +11,86 @@ use Moose;
 use namespace::autoclean;
 
 with (
+    'Dist::Zilla::Role::BeforeBuild',
     'Dist::Zilla::Role::FileMunger',
     'Dist::Zilla::Role::FileFinderUser' => {
         default_finders => [':InstallModules', ':ExecFiles'],
     },
 );
 
+use Data::Dump qw(dump);
+use File::Spec::Functions qw(catfile);
 use Perinci::Sub::Normalize qw(normalize_function_metadata);
+
+sub _get_abstract_from_meta {
+    my ($self, $filename) = @_;
+
+    local @INC = @INC;
+    unshift @INC, 'lib';
+
+    my $content = do {
+        open my($fh), "<", $filename or die "Can't open $filename: $!";
+        local $/;
+        ~~<$fh>;
+    };
+
+    # find out the package of the file
+    my $pkg;
+    if ($content =~ m{^\s*package\s+(\w+(?:::\w+)*)\s*;}m) {
+        $pkg = $1;
+    } else {
+        $pkg = 'main';
+    }
+
+    # XXX if script, do()
+    (my $mod_p = $filename) =~ s!^lib/!!;
+    require $mod_p;
+
+    no strict 'refs';
+    my $metas = \%{"$pkg\::SPEC"};
+
+    my $abstract;
+    {
+        if ($metas->{':package'}) {
+            $abstract = $metas->{':package'}{summary};
+            last if $abstract;
+        }
+
+        # list functions, sorted by the length of its metadata dump
+        my @funcs =
+            map {$_->[0]}
+                sort {length($a->[1]) <=> length($b->[1])}
+                    map { [$_, dump($metas->{$_})] }
+                        grep {/\A\w+\z/} keys %$metas;
+        if (@funcs) {
+            $abstract = $metas->{ $funcs[0] }{summary};
+            last if $abstract;
+        }
+    }
+
+    #$self->log_debug(["Figured out abstract for %s: %s", $filename, $abstract])
+    #    if $abstract;
+    $abstract;
+}
+
+# btw, why does dzil need to know abstract for main module before build?
+sub before_build {
+   my $self  = shift;
+   my $name  = $self->zilla->name;
+   my $class = $name; $class =~ s{ [\-] }{::}gmx;
+   my $filename = $self->zilla->_main_module_override ||
+       catfile( 'lib', split m{ [\-] }mx, "${name}.pm" );
+
+   $filename or die 'No main module specified';
+   -f $filename or die "Path ${filename} does not exist or not a file";
+   open my $fh, '<', $filename or die "File ${filename} cannot open: $!";
+
+   my $abstract = $self->_get_abstract_from_meta($filename);
+   die "Can't get abstract for main module " . $filename unless $abstract;
+
+   $self->zilla->abstract($abstract);
+   return;
+}
 
 sub munge_files {
     my $self = shift;
@@ -35,7 +108,7 @@ sub munge_file {
         return;
     }
 
-    unless ($content =~ m{^#\s*ABSTRACT:\s*(.*?)\s*$}m) {
+    unless ($content =~ m{^#[ \t]*ABSTRACT:[ \t]*([^\n]*)[ \t]*$}m) {
         $self->log_debug(["skipping %s: no # ABSTRACT directive found", $file->name]);
         return;
     }
@@ -46,37 +119,13 @@ sub munge_file {
         return;
     }
 
-    # find the appropriate abstract
-    {
-        local @INC = @INC;
-        unshift @INC, 'lib';
-
-        # find out the package of the file
-        my $package;
-        if ($content =~ m{^\s*package\s+(\w+(?:::\w+)*)\s*;}m) {
-            $package = $1;
-        } else {
-            $package = 'main';
-        }
-
-        # XXX if script, do()
-        (my $mod_p = $file->name) =~ s!^lib/!!;
-        require $mod_p;
-
-        no strict 'refs';
-        my $metas = \%{"$pkg\::SPEC"};
-
-        if ($metas->{':package'}{summary}) {
-            $abstract = $metas->{':package'}{summary};
-            last;
-        }
-    }
+    $abstract = $self->_get_abstract_from_meta($file->name);
 
     unless (defined $abstract) {
         die "Can't figure out abstract for " . $file->name;
     }
 
-    $content =~ s{^#\s*ABSTRACT:.*}{# ABSTRACT: $summary}m
+    $content =~ s{^#\s*ABSTRACT:.*}{# ABSTRACT: $abstract}m
         or die "Can't insert abstract for " . $file->name;
     $self->log(["inserting abstract for %s (%s)", $file->name, $abstract]);
     $file->content($content);
